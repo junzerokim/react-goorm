f3049c2e28f349e430c641337d3f4fb5f411671c
Cos Kim <junzerokim@gmail.com>
2023-02-14 13:01:55 +0900

    Add: 공통 colors 설정

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,14 +1,14 @@
 const colors = {
   WHITE: '#ffffff',
-  BLACK: '#2b2d36',
-  BLACK1: '#313338',
-  BLACK2: '#3e404c',
-  BLACK3: '#525463',
-  BLUE: '#5094fa',
+  BLACK_X1: '#525463',
+  BLACK_X2: '#3e404c',
+  BLACK_X3: '#313338',
+  BLACK_X4: '#2b2d36',
+  BLUE_X1: '#5094fa',
+  BLUE_X2: '#0b53bd',
+  BLUE_X3: '#004d8c',
   RED: '#f5535e',
   YELLOW: '#ffc107',
-  NAVY: '#0b53bd',
-  NAVY1: '#004d8c',
 };
 
 export default colors;
