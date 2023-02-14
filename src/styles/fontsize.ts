3f8ac1c0e0cfc1a958b68eedf2fe8785cefdbbb1
Cos Kim <junzerokim@gmail.com>
2023-02-14 13:54:49 +0900

    Add: 공통 font size 설정

---
+++ src/styles/fontsize.ts
@@ -0,0 +1,9 @@
+const fontsize = {
+  xsmall: '12px',
+  small: '14px',
+  regular: '16px',
+  medium: '20px',
+  large: '24px',
+};
+
+export default fontsize;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,3 @@
 export { default as colors } from './colors';
+export { default as fontsize } from './fontsize';
 export { default as GlobalStyle } from './GlobalStyle';
