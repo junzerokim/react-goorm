b8cf07601f2c487bd5ae0f9197287b1614b0f9bc
Cos Kim <junzerokim@gmail.com>
2023-02-17 02:56:08 +0900

    Add: WrapperLayout Component

--- src/components/template/WrapperLayout/index.tsx
+++ src/components/template/WrapperLayout/index.tsx
@@ -1,5 +1,17 @@
-function WrapperLayout() {
-  return null;
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+import { flex } from 'styles';
+
+interface IWrapperLayout {
+  children: ReactNode;
+}
+
+function WrapperLayout({ children }: IWrapperLayout) {
+  return <Wrapper>{children}</Wrapper>;
 }
 
 export default WrapperLayout;
+
+const Wrapper = styled.div`
+  ${flex('space-between', '')};
+`;
