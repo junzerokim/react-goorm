1a8cf2a4bb35881eb2a732b708f27fd4edf57771
Cos Kim <junzerokim@gmail.com>
2023-02-16 23:03:10 +0900

    Add: Header

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,4 @@
 export { default as colors } from './colors';
 export { fontSize, font } from './font';
 export { default as GlobalStyle } from './GlobalStyle';
+export { default as flex } from './flex';
